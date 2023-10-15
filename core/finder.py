c4128bdc9c3724e189efb69db3fb0f95c72868b8
승연 <pioooop2@gmail.com>
2023-10-15 06:58:40 +0000

    add ballfinder

---
+++ core/finder.py
@@ -0,0 +1,22 @@
+"""Find Ball in array image"""
+
+from __future__ import annotations
+
+from numpy.typing import NDArray
+
+from model.vector import Vec2D
+from config.config import get_config
+
+
+class BallFinder:
+    """Detect Ball in 3D array image"""
+
+    def __init__(self) -> None:
+        config = get_config()
+        self._r_range = config["detector"]["r"]
+        self._g_range = config["detector"]["g"]
+        self._b_range = config["detector"]["b"]
+
+    def find_pos(self, image: NDArray) -> Vec2D:
+        """calc the position of the ball using the array image"""
+        raise NotImplementedError
