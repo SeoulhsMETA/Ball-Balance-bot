854fb958d86f54c677c68601b1e352d9fe9e54eb
승연 <pioooop2@gmail.com>
2023-10-15 07:21:34 +0000

    add cycledlist type

--- model/cycledlist.py
+++ model/cycledlist.py
@@ -1,30 +1,31 @@
 """Cycled List data structure"""
 
 from __future__ import annotations
-from typing import Any
+from typing import TypeVar
 from collections.abc import Iterator, Sequence
 
+T = TypeVar("T")
 
-class CycledList(list):
+class CycledList(list[T]):
     """Cycled list"""
 
     def __iter__(self) -> CycledListIterator:
         return CycledListIterator(self)
 
 
-class CycledListIterator(Iterator):
+class CycledListIterator(Iterator[T]):
     """Iterator for CycledList"""
 
     @property
-    def current(self) -> Any:
+    def current(self) -> T:
         """get current indexed data"""
         return self._data[self._index]
 
-    def __init__(self, data: Sequence[Any]) -> None:
+    def __init__(self, data: Sequence[T]) -> None:
         super().__init__()
         self._index = 0
         self._data = tuple(data)
 
-    def __next__(self) -> Any:
+    def __next__(self) -> T:
         self._index = (self._index + 1) % len(self._data)
         return self._data[self._index - 1]
