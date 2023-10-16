e9a19fd8e317ce9263b6083886c9a77bcf38c662
승연 <pioooop2@gmail.com>
2023-10-17 08:48:21 +0900

    Update config.py for python 3.11

--- config/config.py
+++ config/config.py
@@ -6,8 +6,10 @@ from typing import Any
 CONFIG_PATH = "config.toml"
 
 if version_info.major == 3 and version_info.minor >= 11:
-    # import tomllib
-    pass
+    import tomllib
+    
+    with open(CONFIG_PATH, "br", encoding="utf-8") as f:
+        _config = tomllib.load(f)
 else:
     import toml
 
