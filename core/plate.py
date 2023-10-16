036d969d44e87aa7a74af6f5825b859c911e7307
piop2 <pioooop2@gmail.com>
2023-10-16 23:28:14 +0900

    refactoring config key

--- core/plate.py
+++ core/plate.py
@@ -12,8 +12,8 @@ class PlateController:
 
     def __init__(self) -> None:
         config = get_config()
-        self.servo_x = AngularServo(pin=config["servo"]["x_axis_pin"])
-        self.servo_y = AngularServo(pin=config["servo"]["x_axis_pin"])
+        self.servo_x = AngularServo(pin=config["plate"]["servo_x_pin"])
+        self.servo_y = AngularServo(pin=config["plate"]["servo_y_pin"])
 
     #Rotate servo absolutely
     def tilt_to(self,xAngle,yAngle) -> None:
