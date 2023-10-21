b267eada15977e4c584595081fcd7725fcebde3d
ehdrjs13 <ehdrjs070102@gmail.com>
2023-10-21 13:03:35 +0900

    code style black

--- core/finder.py
+++ core/finder.py
@@ -11,32 +11,34 @@ from config.config import get_config
 class BallFinder:
     """Detect Ball in 3D array image"""
 
-    def __init__(self, r_range: list[int, int], g_range: list[int, int], b_range: list[int, int]) -> None:
+    def __init__(
+        self, r_range: list[int, int], g_range: list[int, int], b_range: list[int, int]
+    ) -> None:
+        self._r_range = range(r_range[0], r_range[1] + 1)
+        self._g_range = range(g_range[0], g_range[1] + 1)
+        self._b_range = range(b_range[0], b_range[1] + 1)
 
-        self._r_range = range(r_range[0],r_range[1]+1)
-        self._g_range = range(g_range[0],g_range[1]+1)
-        self._b_range = range(b_range[0],b_range[1]+1)
-    
     @classmethod
     def from_config(cls) -> BallFinder:
         from config.config import get_config
 
         config = get_config()
-        _r_range = range(config["detector"]["r"][0],config["detector"]["r"][1]+1)
-        _g_range = range(config["detector"]["g"][0],config["detector"]["g"][1]+1)
-        _b_range = range(config["detector"]["b"][0],config["detector"]["b"][1]+1)
+        _r_range = range(config["detector"]["r"][0], config["detector"]["r"][1] + 1)
+        _g_range = range(config["detector"]["g"][0], config["detector"]["g"][1] + 1)
+        _b_range = range(config["detector"]["b"][0], config["detector"]["b"][1] + 1)
 
-        return cls(_r_range,_g_range,_b_range)
-
-
+        return cls(_r_range, _g_range, _b_range)
 
     def find_pos(self, arr_image: ndarray[ndarray[int, int, int]]) -> Vec2D:
         """calc the position of the ball using the array image"""
-        ball_loc = [] 
+        ball_loc = []
         for y, arr in enumerate(arr_image):
             for x, rgb in enumerate(arr):
-                if rgb[0] in self._r_range and rgb[1] in self._g_range and rgb[2] in self._b_range:
-                    ball_loc.append(Vec2D(x,y))
-        
+                if (
+                    rgb[0] in self._r_range
+                    and rgb[1] in self._g_range
+                    and rgb[2] in self._b_range
+                ):
+                    ball_loc.append(Vec2D(x, y))
+
         return ball_loc
-    
--- core/plate.py
+++ core/plate.py
@@ -11,21 +11,21 @@ class PlateController:
     def __init__(self, servo_x_pin: int, servo_y_pin: int) -> None:
         self.servo_x = AngularServo(pin=servo_x_pin)
         self.servo_y = AngularServo(pin=servo_y_pin)
-    
+
     @classmethod
     def from_config(cls) -> PlateController:
         from config.config import get_config
+
         config = get_config()
-        
+
         return cls(config["plate"]["servo_x_pin"], config["plate"]["servo_y_pin"])
 
-    def tilt_to(self, xAngle:float, yAngle:float) -> None:
+    def tilt_to(self, xAngle: float, yAngle: float) -> None:
         """Rotate servo absolutely"""
         self.servo_x.angle = xAngle
         self.servo_y.angle = yAngle
 
-    
-    def tilt_by(self, d_xAngle:float, d_yAngle:float) -> None:
+    def tilt_by(self, d_xAngle: float, d_yAngle: float) -> None:
         """Rotate servo relatively"""
         self.servo_x.angle += d_xAngle
         self.servo_y.angle += d_yAngle
