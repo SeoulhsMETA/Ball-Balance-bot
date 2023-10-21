9915c5440730555ed2388f1ec1f9ccb8634365a0
ehdrjs13 <ehdrjs070102@gmail.com>
2023-10-21 12:06:11 +0900

    finder: add typehint

--- core/plate.py
+++ core/plate.py
@@ -15,13 +15,13 @@ class PlateController:
         self.servo_x = AngularServo(pin=config["plate"]["servo_x_pin"])
         self.servo_y = AngularServo(pin=config["plate"]["servo_y_pin"])
 
-    def tilt_to(self, xAngle, yAngle) -> None:
+    def tilt_to(self, xAngle:float, yAngle:float) -> None:
         """Rotate servo absolutely"""
         self.servo_x.angle = xAngle
         self.servo_y.angle = yAngle
 
     
-    def tilt_by(self, d_xAngle, d_yAngle) -> None:
+    def tilt_by(self, d_xAngle:float, d_yAngle:float) -> None:
         """Rotate servo relatively"""
         self.servo_x.angle += d_xAngle
         self.servo_y.angle += d_yAngle
