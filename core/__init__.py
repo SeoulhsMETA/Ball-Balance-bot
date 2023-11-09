7b76dc421ba302af81ce9a7ab40d49776c587e64
piop2 <seungyounyou0220@gmail.com>
2023-11-10 04:14:27 +0900

    core/client.py -> core/__init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
+
+import asyncio
 from typing import NoReturn
 
 from picamera import PiCamera
 from picamera.array import PiRGBArray
+from socketio import AsyncClient
 
 from core.controll import BallControl
 from core.finder import BallFinder
@@ -10,6 +13,7 @@ from core.mode import ModeHandler
 from core.plate import PlateController
 from core.client import SocketIOClient
 from model.status import BotStatusQueue, BotStatus
+from config.config import get_config
 
 
 class Bot:
@@ -38,13 +42,19 @@ class Bot:
             self.status_queue.put(BotStatus.make(ball_pos, self.plate))
 
 
-class Reporter:
+class Client:
     """Bot status reporter"""
 
     def __init__(self, status_queue: BotStatusQueue) -> None:
-        self.client = SocketIOClient.from_config()
-
+        self.url = get_config()["client"]["url"]
         self.status_queue = status_queue
 
     async def run(self) -> NoReturn:
-        return
+        async with AsyncClient() as sio:
+            await sio.connect(self.url)
+
+            while True:
+                await asyncio.sleep(1)
+                if (status_data := self.status_queue.get()) is not None:
+                    await sio.emit("update_status", dict(status_data))
+
